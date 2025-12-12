d90029394fc298df07f67cd8f9410d70a6808443
Adam Jurkiewicz Pythonista Local <adam@jurkiewicz.tech>
2025-12-12 11:00:51 +0100

    static

--- tests/test_fn.py
+++ tests/test_fn.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
-from app import app
+from app import static_function
 
 def test_fn_returns_ok():
     assert static_function() == "OK"
