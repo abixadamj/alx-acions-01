88f23cd83dbefc0c71b18406fafc9d3b87e9351e
Adam Jurkiewicz Pythonista Local <adam@jurkiewicz.tech>
2025-12-12 11:02:13 +0100

    import pytest removed

--- tests/test_fn.py
+++ tests/test_fn.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import pytest
 from app import static_function
 
 def test_fn_returns_ok():
